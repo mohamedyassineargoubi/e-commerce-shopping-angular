9893da055dfa2d818d58867a563e20355ec3f75d
Yassine <mohamedyassine.argoubi@esprit.tn>
2022-01-28 17:31:06 +0100

    components+service

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { CardComponent } from './component/card/card.component';
+import { ProductsComponent } from './component/products/products.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    CardComponent,
+    ProductsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/component/products/products.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/service/api.service';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss']
+})
+export class ProductsComponent implements OnInit {
+  productList: any;
+  constructor(private api: ApiService) { }
+
+  ngOnInit(): void {
+    this.api.getProducts()
+      .subscribe(res => {
+        this.productList = res;
+      })
+  }
+
+}
